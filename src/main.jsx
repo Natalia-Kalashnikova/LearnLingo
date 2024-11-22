0662a1a989bdffa73825de2cae334cffdb843964
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-11-23 01:42:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,30 @@
-import { StrictMode } from 'react';
+// import { StrictMode } from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+// import { createRoot } from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { persistor, store } from './redux/store.js';
+// import App from './components/App/App.jsx';
+// import './index.css';
+// import '../src/services/firebase.js';
+// import { ToastContainer } from 'react-toastify';
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Provider store={store}>
+//       <PersistGate persistor={persistor}>
+//         <BrowserRouter basename="/LearnLingo">
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//       <ToastContainer />
+//     </Provider>
+//   </StrictMode>
+// );
+
+
 import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store.js';
@@ -9,8 +33,7 @@ import './index.css';
 import '../src/services/firebase.js';
 import { ToastContainer } from 'react-toastify';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter basename="/LearnLingo">
@@ -19,5 +42,4 @@ createRoot(document.getElementById('root')).render(
       </PersistGate>
       <ToastContainer />
     </Provider>
-  </StrictMode>
 );
